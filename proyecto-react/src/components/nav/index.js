fe94f90dbd37b1cfd4c99c7f2464a2c4e34bad4c
John Doe <di.mariaocampo@gmail.com>
2020-04-30 21:50:16 -0300

    se trabajo sobre el elemento Nav. falta acomodar menu hamburguesa

--- proyecto-react/src/components/nav/index.js
+++ proyecto-react/src/components/nav/index.js
@@ -2,25 +2,23 @@ import React from 'react';
 import '../../App';
 import '../nav/style-nav.scss';
 
-function Nav(){
-    return (
-        <nav className="nav">
-            <img src ={process.env.PUBLIC_URL + "/images/logoH.png"} className="nav_logo" alt="logo"/> 
-            <input type="checkbox" id="nav_checkMenu"/>
-            <label for="checkMenu" class="nav--btn-show">
-            <i class="fas fa-bars"></i>
+const Nav = () => {
+     return (
+        <nav className="nav_container">
+            <input type="checkbox" className="nav_check-menu"/>
+            <label for="nav_check-menu" class="nav_btn-show">
+                <i class="fas fa-bars"></i>
             </label>
-            <ul class="nav--menu">
-                <li><label for="checkMenu" class="nav--btn-hide">
-                <i class="fas fa-times"></i>
-                </label></li>
-                    <li class="nav--menu_item">Quienes somos</li>
-                    <li class="nav--menu_item">Recetas</li>
-                    <li class="nav--menu_item">Belleza</li>
-                    <li class="nav--menu_item">Noticias</li>
-                    <li class="nav--menu_item">Contactanos</li>
-            </ul>       
-        </nav>
+                <div className="nav_menu">
+                    <ul>
+                        <li class="nav--menu_item">Quienes somos</li>
+                        <li class="nav--menu_item">Recetas</li>
+                        <li class="nav--menu_item">Belleza</li>
+                        <li class="nav--menu_item">Noticias</li>
+                        <li class="nav--menu_item">Contactanos</li>
+                    </ul>
+                </div>       
+            </nav>
     )
 }
 
