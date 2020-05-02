61f6bbaba749a87047174bb74cd8f31ac382f1c2
John Doe <di.mariaocampo@gmail.com>
2020-05-02 00:18:38 -0300

    se termina menú hamburguesa

--- proyecto-react/src/components/nav/index.js
+++ proyecto-react/src/components/nav/index.js
@@ -4,18 +4,18 @@ import '../nav/style-nav.scss';
 
 const Nav = () => {
      return (
-        <nav className="nav_container">
-            <input type="checkbox" className="nav_check-menu"/>
-            <label for="nav_check-menu" class="nav_btn-show">
+        <nav>
+            <input type="checkbox" id="nav_check-menu"/>
+            <label for="nav_check-menu" className="nav_label">
                 <i class="fas fa-bars"></i>
             </label>
                 <div className="nav_menu">
                     <ul>
-                        <li class="nav--menu_item">Quienes somos</li>
-                        <li class="nav--menu_item">Recetas</li>
-                        <li class="nav--menu_item">Belleza</li>
-                        <li class="nav--menu_item">Noticias</li>
-                        <li class="nav--menu_item">Contactanos</li>
+                        <li><a href="www.google.com">Quienes somos</a></li>
+                        <li><a href="www.google.com">Recetas</a></li>
+                        <li><a href="www.google.com">Belleza</a></li>
+                        <li><a href="www.google.com">Noticias</a></li>
+                        <li><a href="www.google.com">Contactanos</a></li>
                     </ul>
                 </div>       
             </nav>
