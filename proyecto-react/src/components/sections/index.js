d73799495129950b4a9146aa8b15fd03f7d9b3ef
John Doe <di.mariaocampo@gmail.com>
2020-05-05 19:11:29 -0300

    Se completó con props el componente Sections

--- proyecto-react/src/components/sections/index.js
+++ proyecto-react/src/components/sections/index.js
@@ -2,30 +2,55 @@ import React from 'react';
 import '../../App';
 import './style-section.scss';
 
+const dataSection =[
+    {
+        classNameDiv:"section_one",
+        tittle:"Fitness en casa",
+        classNameTittle:"section_tittle",
+        src:"/images/fitness.jpg",
+        classNameImg:"section_image",
+        alt:"Mujer fitness",
+        classNameP:"section_text",
+        p:"Abdominales combinados con hombros: 45 segundos x 5 series. Se pueden combinar con mancuernas para agregar peso. Hay mancuernas de diferentes pesos y formas. Los pesajes comienzan en los 1kg y hay hasta 4 kilos para levantar cada una."
+    },
+    {
+        classNameDiv:"section_two",
+        tittle:"Recetas",
+        classNameTittle:"section_tittle",
+        src:"/images/recipe.jpg",
+        classNameImg:"section_image",
+        alt:"plato de comida",
+        classNameP:"section_text",
+        p:"Tener más tiempo en el hogar permite, entre otras cosas, poder dedicarle a la preparación de los platos una atención especial. Al disponer de un plan de alimentación semanal se puede ir al supermercado a hacer una compra consciente para poder tener a mano los ingredientes necesarios."
+    },
+    {
+        classNameDiv:"section_three",
+        tittle:"Belleza",
+        classNameTittle:"section_tittle",
+        src:"/images/belleza.jpg",
+        classNameImg:"section_image",
+        alt:"elementos naturales",
+        classNameP:"section_text",
+        p:"Tener más tiempo en el hogar permite, entre otras cosas, poder dedicarle a la preparación de los platos una atención especial. Al disponer de un plan de alimentación semanal se puede ir al supermercado a hacer una compra consciente para poder tener a mano los ingredientes necesarios."
+    },
+
+
+]
+
+const content = dataSection.map((data) => (
+    <React.Fragment>
+        <div className={data.classNameDiv}>
+            <h3 className={data.classNameTittle}>{data.tittle}</h3>
+                <img src={process.env.PUBLIC_URL + data.src} className={data.classNameImg} alt={data.alt}/>
+                <p className={data.classNameP}>{data.p}</p>
+        </div>
+    </React.Fragment>
+))
+
 const Sections = () =>{
     return(
         <section className="section">
-            <div className="section_one">
-                <h3 className="section_tittle">Fitness en casa</h3>
-                    <img src = {process.env.PUBLIC_URL + "/images/fitness.jpg"} className="section_image" alt="mujer fitness"/>
-                    <p className="section_text">
-                        Abdominales combinados con hombros: 45 segundos x 5 series. Se pueden combinar con mancuernas para agregar peso. Hay mancuernas de diferentes pesos y formas. Los pesajes comienzan en los 1kg y hay hasta 4 kilos para levantar cada una.
-                    </p>
-            </div>
-            <div className="section_two">
-                <h3 className="section_tittle">Recetas</h3>
-                    <img src = {process.env.PUBLIC_URL + "/images/recipe.jpg"} className="section_image" alt="plato de comida"/>
-                    <p className="section_text">
-                        Tener más tiempo en el hogar permite, entre otras cosas, poder dedicarle a la preparación de los platos una atención especial. Al disponer de un plan de alimentación semanal se puede ir al supermercado a hacer una compra consciente para poder tener a mano los ingredientes necesarios.
-                    </p>
-            </div>
-            <div className="section_three">
-                <h3 className="section_tittle">Belleza</h3>
-                    <img src ={process.env.PUBLIC_URL + "/images/belleza.jpg"} className="section_image" alt="elementos naturales"/>
-                    <p className="section_text">
-                        No importa si tu piel es seca, normal, o grasosa. No tienes que invertir en muchas cremas, con un sólo producto que sea bueno puedes tener una piel de ensueño. 
-                   </p>
-            </div>
+            {content}                    
         </section>
     )
 }
