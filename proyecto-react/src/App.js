c3bb3557420e0aab40bd8f934f988c9a592b8b4b
John Doe <di.mariaocampo@gmail.com>
2020-05-07 10:44:20 -0300

    actualizacion del index con Router

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter,Route, Switch } from 'react-router-dom';
 import Home from './pages/home';
 import Recipes from './pages/recipes';
 
 const App = () =>{
   return (
       <BrowserRouter>
-        <Route exact path="/home" component={Home}/>
-        <Route exact path="/recipes" component={Recipes}/>         
+        <Switch>
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/recipes" component={Recipes}/> 
+        </Switch>          
       </BrowserRouter>   
   )
 }
