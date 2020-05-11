5e4ebaa8a50581693c6217c74ccaca77abd461d2
John Doe <di.mariaocampo@gmail.com>
2020-05-11 16:17:44 -0300

    cambios en el form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter,Route, Switch } from 'react-router-dom';
 import Home from './pages/home';
 import Article from './pages/article';
 import FormPage from './pages/form';
+import Comments from './pages/comments';
 
 const App = () =>{
   return (
@@ -11,6 +12,7 @@ const App = () =>{
           <Route exact path="/" component={Home}/>
           <Route exact path="/article" component={Article}/> 
           <Route exact path="/form" component={FormPage}/> 
+          <Route exact path="/comments" component={Comments}/>
         </Switch>          
       </BrowserRouter>   
   )
--- src/pages/comments.js
+++ src/pages/comments.js
@@ -1,33 +1,49 @@
-import React from 'react';
-import useForm from '../../services/form';
-import '../form/style-form.scss';
+import React, { useState } from 'react';
+import Layout from '../components/layout';
+import useForm from '../services/form';
+import CardComments from '../components/comments'
 
-const FormContact = () => {
 
-    const[values,handleChange] = useForm();
+const Comments = () =>{
+    // const[values,handleChange] = useForm();
+    
+    const[comments, setComments] = useState([])
+    // // const [suma, setSuma] = useState(0)
 
     const handleSubmit = e =>{
         e.preventDefault()
-        console.log({values})
-
+        let auxArray = comments
+        // comments.map(comment => auxArray.push(comment, {firstName}))
+        auxArray.push({firstName})
+        console.log(auxArray)
+        setComments(auxArray)
+        // console.log(values)
+        // setComments( prevState => ({comments: [values, ...prevState.comments]}))
+        // console.log(comments)
+        // setSuma(prevState => ({suma: prevState.suma + 1}))
     }
+    const [firstName, setFirstName] = useState('')
 
+    const handleInput = ({ target }) => setFirstName(target.value) 
+    console.log(comments)
     return(
-    <React.Fragment>
-        <h1 className="form__title">¡Dejanos tu Comentario!</h1>
+        <Layout>
+            <div>
+            <h1 className="form__title">¡Dejanos tu Comentario!</h1>
             <form onSubmit ={handleSubmit} className="form">
                 <div className="form__label__container">
                     <label className="form__label">Nombre</label>
                         <input 
                         type="text" 
                         name="firstName" 
-                        onChange={handleChange} 
-                        value={values.firstName||""}
+                        // onChange={handleChange} 
+                        onChange={handleInput}
+                        value={firstName}
                         placeholder="Ingresa tu nombre"
                         className="form__input"  
                         required />
                 </div>
-                <div className="form__label__container">   
+                {/* <div className="form__label__container">   
                     <label className="form__label">Apellido</label>
                         <input 
                         type="text" 
@@ -48,8 +64,8 @@ const FormContact = () => {
                         placeholder="ejemplo@ejemplo.com"
                         className="form__input-mail"  
                         required />
-                </div>
-                <div className="form__textarea">        
+                </div> */}
+                {/* <div className="form__textarea">        
                     <label className="form__label">Dejanos tu consulta:</label>
                         <textarea 
                         className="form__textarea--input"
@@ -60,13 +76,16 @@ const FormContact = () => {
                         placeholder="Esperamos tu consulta o comentario!"
                         required 
                         />
-                    </div>
+                    </div> */}
                     <div className="form__button__container">    
                         <button type="submit" className="form__button">Enviar</button>
                     </div>    
             </form>
-    </React.Fragment>
+                 <CardComments values={comments} />
+            </div>
+        </Layout>
     )
+
 }
 
-export default FormContact
+export default Comments
